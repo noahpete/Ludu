c875ba976e5ed54433274fc0bcff9cae597f5deb
Noah Peters <97405316+noahpete@users.noreply.github.com>
2024-06-25 16:47:45 -0400

    External untracked content updates

--- Ludu/external/imgui/cmake_install.cmake
+++ Ludu/external/imgui/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/Noah/projects/Ludu/external/imgui
+# Install script for directory: C:/Users/Noah/projects/Ludu/Ludu/external/imgui
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files/Ludu")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Ludu")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -30,58 +30,5 @@ endif()
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include/imgui_src" TYPE FILE FILES
-    "C:/Users/Noah/projects/Ludu/external/imgui/imgui/imgui.cpp"
-    "C:/Users/Noah/projects/Ludu/external/imgui/imgui/stb_rect_pack.h"
-    "C:/Users/Noah/projects/Ludu/external/imgui/imgui/stb_textedit.h"
-    "C:/Users/Noah/projects/Ludu/external/imgui/imgui/stb_truetype.h"
-    )
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  if(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Dd][Ee][Bb][Uu][Gg])$")
-    include("C:/Users/Noah/projects/Ludu/external/imgui/CMakeFiles/imgui.dir/install-cxx-module-bmi-Debug.cmake" OPTIONAL)
-  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Rr][Ee][Ll][Ee][Aa][Ss][Ee])$")
-    include("C:/Users/Noah/projects/Ludu/external/imgui/CMakeFiles/imgui.dir/install-cxx-module-bmi-Release.cmake" OPTIONAL)
-  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Mm][Ii][Nn][Ss][Ii][Zz][Ee][Rr][Ee][Ll])$")
-    include("C:/Users/Noah/projects/Ludu/external/imgui/CMakeFiles/imgui.dir/install-cxx-module-bmi-MinSizeRel.cmake" OPTIONAL)
-  elseif(CMAKE_INSTALL_CONFIG_NAME MATCHES "^([Rr][Ee][Ll][Ww][Ii][Tt][Hh][Dd][Ee][Bb][Ii][Nn][Ff][Oo])$")
-    include("C:/Users/Noah/projects/Ludu/external/imgui/CMakeFiles/imgui.dir/install-cxx-module-bmi-RelWithDebInfo.cmake" OPTIONAL)
-  endif()
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/imgui/imgui-targets.cmake")
-    file(DIFFERENT _cmake_export_file_changed FILES
-         "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/imgui/imgui-targets.cmake"
-         "C:/Users/Noah/projects/Ludu/external/imgui/CMakeFiles/Export/1c9f1db1a0c9398944d65050189647a4/imgui-targets.cmake")
-    if(_cmake_export_file_changed)
-      file(GLOB _cmake_old_config_files "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/imgui/imgui-targets-*.cmake")
-      if(_cmake_old_config_files)
-        string(REPLACE ";" ", " _cmake_old_config_files_text "${_cmake_old_config_files}")
-        message(STATUS "Old export file \"$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cmake/imgui/imgui-targets.cmake\" will be replaced.  Removing files [${_cmake_old_config_files_text}].")
-        unset(_cmake_old_config_files_text)
-        file(REMOVE ${_cmake_old_config_files})
-      endif()
-      unset(_cmake_old_config_files)
-    endif()
-    unset(_cmake_export_file_changed)
-  endif()
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/imgui" TYPE FILE FILES "C:/Users/Noah/projects/Ludu/external/imgui/CMakeFiles/Export/1c9f1db1a0c9398944d65050189647a4/imgui-targets.cmake")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include" TYPE FILE FILES "C:/Users/Noah/projects/Ludu/external/imgui/imgui/imgui.h")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/include" TYPE FILE RENAME "imconfig-sample.h" FILES "C:/Users/Noah/projects/Ludu/external/imgui/imgui/imconfig.h")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cmake/imgui" TYPE FILE FILES "C:/Users/Noah/projects/Ludu/external/imgui/hide/imgui-config.cmake")
 endif()
 
