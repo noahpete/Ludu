7df809454b8b600644e3dfb3293e9e87be867092
Noah Peters <97405316+noahpete@users.noreply.github.com>
2024-06-25 22:05:56 -0400

    Ludu keycodes and mousecodes

---
+++ Ludu/src/Core/KeyCodes.h
@@ -0,0 +1,125 @@
+#pragma once
+
+namespace Ludu
+{
+    enum KeyCode
+    {
+        None,
+        Space,
+        Apostrophe, /* ' */
+        Comma,     /* , */
+        Minus,     /* - */
+        Period,    /* . */
+        Slash,     /* / */
+
+        D0, /* 0 */
+        D1, /* 1 */
+        D2, /* 2 */
+        D3, /* 3 */
+        D4, /* 4 */
+        D5, /* 5 */
+        D6, /* 6 */
+        D7, /* 7 */
+        D8, /* 8 */
+        D9, /* 9 */
+
+        Semicolon, /* ; */
+        Equal,     /* = */
+
+        A,
+        B,
+        C,
+        D,
+        E,
+        F,
+        G,
+        H,
+        I,
+        J,
+        K,
+        L,
+        M,
+        N,
+        O,
+        P,
+        Q,
+        R,
+        S,
+        T,
+        U,
+        V,
+        W,
+        X,
+        Y,
+        Z,
+
+        LeftBracket,  /* [ */
+        Backslash,    /* \ */
+        RightBracket, /* ] */
+        GraveAccent,  /* ` */
+
+        /* Function keys */
+        Escape,
+        Enter,
+        Tab,
+        Backspace,
+        Insert,
+        Delete,
+        Right,
+        Left,
+        Down,
+        Up,
+        PageUp,
+        PageDown,
+        Home,
+        End,
+        CapsLock,
+        ScrollLock,
+        NumLock,
+        PrintScreen,
+        Pause,
+        F1,
+        F2,
+        F3,
+        F4,
+        F5,
+        F6,
+        F7,
+        F8,
+        F9,
+        F10,
+        F11,
+        F12,
+
+        /* Keypad */
+        KP0,
+        KP1,
+        KP2,
+        KP3,
+        KP4,
+        KP5,
+        KP6,
+        KP7,
+        KP8,
+        KP9,
+        KPDecimal,
+        KPDivide,
+        KPMultiply,
+        KPSubtract,
+        KPAdd,
+        KPEnter,
+        KPEqual,
+
+        LeftShift,
+        LeftControl,
+        LeftAlt,
+        LeftSuper,
+        RightShift,
+        RightControl,
+        RightAlt,
+        RightSuper,
+        Menu,
+
+        END
+    };
+}
---
+++ Ludu/src/Core/MouseCodes.h
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace Ludu {
+
+    enum MouseCode
+    {
+        Button0,
+        Button1,
+        Button2,
+        Button3,
+        Button4,
+        Button5,
+        Button6,
+
+        ButtonLast,
+        ButtonLeft,
+        ButtonRight,
+        ButtonMiddle
+    };
+}
