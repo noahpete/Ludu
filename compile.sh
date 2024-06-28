4b9df1642b9d21e313ffc67732ff36ac879dc74e
Noah Peters <nope@umich.edu>
2024-06-28 15:24:27 -0400

    Changed shader paths

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
-/Users/noah/VulkanSDK/1.3.283.0/macOS/bin/glslc Sandbox/Assets/Shaders/simple_shader.vert -o Sandbox/Assets/Shaders/simple_shader.vert.spv
-/Users/noah/VulkanSDK/1.3.283.0/macOS/bin/glslc Sandbox/Assets/Shaders/simple_shader.frag -o Sandbox/Assets/Shaders/simple_shader.frag.spv
+/Users/noah/VulkanSDK/1.3.283.0/macOS/bin/glslc simple_shader.vert -o simple_shader.vert.spv
+/Users/noah/VulkanSDK/1.3.283.0/macOS/bin/glslc simple_shader.frag -o simple_shader.frag.spv
