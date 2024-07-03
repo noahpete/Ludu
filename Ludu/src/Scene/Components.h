72c99cf6e094bfec9eb7b552f661cde81c013460
Noah Peters <nope@umich.edu>
2024-07-03 16:19:04 -0400

    2D transform components

--- Ludu/src/Scene/Components.h
+++ Ludu/src/Scene/Components.h
@@ -2,20 +2,21 @@
 
 #include "ldpch.h"
 #include <glm/glm.hpp>
-//#include "Renderer/Camera.h"
-//#include "Scene/ScriptableEntity.h"
+// #include "Renderer/Camera.h"
+// #include "Scene/ScriptableEntity.h"
 
 #include "Platform/Vulkan/VulkanModel.h"
 
-namespace Ludu {
+namespace Ludu
+{
 
     struct TagComponent
     {
         std::string Tag;
 
         TagComponent() = default;
-        TagComponent(const TagComponent&) = default;
-        TagComponent(const std::string& tag)
+        TagComponent(const TagComponent &) = default;
+        TagComponent(const std::string &tag)
             : Tag(tag) {}
     };
 
@@ -26,49 +27,58 @@ namespace Ludu {
         float Rotation;
 
         glm::vec2 Translation2D{};
-        glm::mat2 GetMat2() { return glm::mat2{1.0f}; }
+        glm::vec2 Scale2D{1.0f, 1.0f};
+        float Rotation2D;
+        glm::mat2 GetMat2()
+        {
+            const float s = glm::sin(Rotation2D);
+            const float c = glm::cos(Rotation2D);
+            glm::mat2 rotMatrix{{c, s}, {-s, c}};
+
+            glm::mat2 scaleMat{{Scale2D.x, 0.0f}, {0.0f, Scale2D.y}};
+            return rotMatrix * scaleMat;
+        }
 
         TransformComponent() = default;
-        TransformComponent(const TransformComponent&) = default;
+        TransformComponent(const TransformComponent &) = default;
         TransformComponent(const glm::vec2 position, glm::vec2 scale, float rotation)
             : Position(position), Scale(scale), Rotation(rotation) {}
     };
 
     struct SpriteRendererComponent
     {
-        glm::vec4 Color{ 1.0f, 1.0f, 1.0f, 1.0f };
+        glm::vec4 Color{1.0f, 1.0f, 1.0f, 1.0f};
 
         SpriteRendererComponent() = default;
-        SpriteRendererComponent(const SpriteRendererComponent&) = default;
-        SpriteRendererComponent(const glm::vec4& color)
+        SpriteRendererComponent(const SpriteRendererComponent &) = default;
+        SpriteRendererComponent(const glm::vec4 &color)
             : Color(color) {}
     };
 
     struct MeshComponent
     {
         Ref<VulkanModel> Model;
-        glm::vec3 Color = { 1.0f, 1.0f, 1.0f };
+        glm::vec3 Color = {1.0f, 1.0f, 1.0f};
 
-        MeshComponent(std::vector<Ludu::Vertex>& vertices)
+        MeshComponent(std::vector<Ludu::Vertex> &vertices)
         {
             Model = CreateRef<VulkanModel>(vertices);
         }
     };
 
-    //struct CameraComponent
+    // struct CameraComponent
     //{
-    //    Camera Camera;
-    //    bool Primary = true;
+    //     Camera Camera;
+    //     bool Primary = true;
 
     //    CameraComponent() = default;
     //    CameraComponent(const CameraComponent&) = default;
     //    // CameraComponent(const )
     //};
 
-    //struct NativeScriptComponent
+    // struct NativeScriptComponent
     //{
-    //    ScriptableEntity* Instance = nullptr;
-
+    //     ScriptableEntity* Instance = nullptr;
 
     //    ScriptableEntity* (*InstantiateScript)();
     //    void (*DestroyScript)(NativeScriptComponent*);
