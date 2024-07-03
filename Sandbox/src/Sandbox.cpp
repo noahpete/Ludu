8b63710d831e8193e6cae95dd8708c44573552d2
Noah Peters <nope@umich.edu>
2024-07-03 16:06:47 -0400

    Moved playground to Sandbox

--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -2,21 +2,49 @@
 
 #include <iostream>
 
+class ExampleLayer : public Ludu::Layer
+{
+public:
+	ExampleLayer()
+	{
+		m_Entity = m_Scene.CreateEntity("Example");
+
+		std::vector<Ludu::Vertex> vertices{
+			{{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
+			{{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
+			{{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}};
+
+		m_Entity.AddComponent<Ludu::MeshComponent>(vertices);
+	}
+
+	~ExampleLayer()
+	{
+	}
+
+	void OnUpdate(Ludu::Timestep ts) override
+	{
+		Ludu::Renderer::Get().Submit(&m_Entity);
+	}
+
+private:
+	Ludu::Scene m_Scene;
+	Ludu::Entity m_Entity;
+};
+
 class Sandbox : public Ludu::Application
 {
 public:
 	Sandbox()
 	{
+		PushLayer(new ExampleLayer());
 	}
 
 	~Sandbox()
 	{
-
 	}
-
 };
 
-Ludu::Application* Ludu::CreateApplication()
+Ludu::Application *Ludu::CreateApplication()
 {
 	return new Sandbox();
 }
