75b9bd328a8840665f89fd20f6ef002084e44d13
Noah Peters <97405316+noahpete@users.noreply.github.com>
2024-07-06 13:46:29 -0400

    Normal transform test

--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -8,52 +8,15 @@ public:
 	ExampleLayer()
 	{
 		m_Entity = m_Scene.CreateEntity("Example");
+		m_SecondEntity = m_Scene.CreateEntity("Example2");
 
-        //std::vector<Ludu::Vertex> vertices{
-        //    // left face (white)
-        //    {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
-        //    {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
-        //    {{-.5f, -.5f, .5f}, {.9f, .9f, .9f}},
-        //    {{-.5f, .5f, -.5f}, {.9f, .9f, .9f}},
+        m_Entity.AddComponent<Ludu::MeshComponent>("flat_vase.obj");
+        m_Entity.GetComponent<Ludu::TransformComponent>().Translation = { -0.3f, 0.2f, 1.5f };
+        m_Entity.GetComponent<Ludu::TransformComponent>().Scale = { 1.0f, 1.0f, 1.0f };
 
-        //    // right face (yellow)
-        //    {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
-        //    {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
-        //    {{.5f, -.5f, .5f}, {.8f, .8f, .1f}},
-        //    {{.5f, .5f, -.5f}, {.8f, .8f, .1f}},
-
-        //    // top face (orange, remember y axis points down)
-        //    {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
-        //    {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
-        //    {{-.5f, -.5f, .5f}, {.9f, .6f, .1f}},
-        //    {{.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
-
-        //    // bottom face (red)
-        //    {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
-        //    {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
-        //    {{-.5f, .5f, .5f}, {.8f, .1f, .1f}},
-        //    {{.5f, .5f, -.5f}, {.8f, .1f, .1f}},
-
-        //    // nose face (blue)
-        //    {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
-        //    {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
-        //    {{-.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
-        //    {{.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
-
-        //    // tail face (green)
-        //    {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
-        //    {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
-        //    {{-.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
-        //    {{.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
-        //};
-
-        //std::vector<uint32_t> indices{ 0,  1,  2,  0,  3,  1,  4,  5,  6,  4,  7,  5,  8,  9,  10, 8,  11, 9,
-        //                  12, 13, 14, 12, 15, 13, 16, 17, 18, 16, 19, 17, 20, 21, 22, 20, 23, 21 };
-
-        m_Entity.AddComponent<Ludu::MeshComponent>("smooth_vase.obj");
-        m_Entity.GetComponent<Ludu::TransformComponent>().Translation = { 0.0f, 0.0f, 2.5f };
-        m_Entity.GetComponent<Ludu::TransformComponent>().Scale = { 0.5f, 0.5f, 0.5f };
-
+        m_SecondEntity.AddComponent<Ludu::MeshComponent>("flat_vase.obj");
+        m_SecondEntity.GetComponent<Ludu::TransformComponent>().Translation = { 0.3f, 0.2f, 1.5f };
+        m_SecondEntity.GetComponent<Ludu::TransformComponent>().Scale = { 2.0f, 0.7f, 1.0f };
 	}
 
 	~ExampleLayer()
@@ -63,13 +26,15 @@ public:
 	void OnUpdate(Ludu::Timestep ts) override
 	{
 		Ludu::Renderer::Get().Submit(&m_Entity);
-        m_Entity.GetComponent<Ludu::TransformComponent>().Rotation.y += 1.0f * ts;
-        m_Entity.GetComponent<Ludu::TransformComponent>().Rotation.x += 0.5f * ts;
+		Ludu::Renderer::Get().Submit(&m_SecondEntity);
+        /*m_Entity.GetComponent<Ludu::TransformComponent>().Rotation.y += 1.0f * ts;
+        m_Entity.GetComponent<Ludu::TransformComponent>().Rotation.x += 0.5f * ts;*/
 	}
 
 private:
 	Ludu::Scene m_Scene;
 	Ludu::Entity m_Entity;
+    Ludu::Entity m_SecondEntity;
 };
 
 class Sandbox : public Ludu::Application
